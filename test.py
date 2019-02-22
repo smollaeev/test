de1b5734166d31280cdc1b3bfe22f3625e1c1241
smollaeev <s.mollaee.v@gamil.com>
2019-02-22 09:33:37 -0800

    modified a bit

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():    
-    x=np.linspace(0,2*np.pi,1000)
+    x=np.linspace(0,2*np.pi,2000)
     y=np.sin(x)
     plt.plot(x,y)
     plt.show()
